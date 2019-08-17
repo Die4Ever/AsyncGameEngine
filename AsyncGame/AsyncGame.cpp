868116be74e5c632bc561b80c294f09f4b8e6ffb
Die4Ever <die4ever2006@hotmail.com>
2019-08-17 01:06:14 -0500

    rewriting library to use a shared circular queue of messages

--- AsyncGame/AsyncGame.cpp
+++ AsyncGame/AsyncGame.cpp
@@ -1,6 +1,3 @@
-// AsyncGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <Windows.h>
 #include <tchar.h>
@@ -8,42 +5,16 @@
 
 #include "../AsyncGameEngine/AsyncGame.h"
 
-void Update(SharedMemory &sm)
-{
-	char buff[1024];
-	auto commands_in = sm.GetNextCommandsFromEngine();
-	CommandBlock commands_out;
-	for(auto &c : commands_in.commands) {
-		sprintf_s(buff, 1024, "got %s", c.command.c_str());
-		Command new_c;
-		new_c.command = buff;
-		commands_out.commands.push_back(new_c);
-	}
-	sm.SendCommandsToEngine(commands_out);
-}
-
 int main(int argc, char* argv[])
 {
-    std::cout << "\n\nGame "<<argv[1]<<"!\n";
+    std::cout << "\n\nGame "<<argv[0]<<" "<<argv[1]<<"!\n";
 
 	int shared_mem_id = 0;
 	sscanf_s(argv[1], "%d", &shared_mem_id);
-	SharedMemory sm;
-	sm.OpenSharedMemory(shared_mem_id);
-	while (1) {
-		Update(sm);
-		//Sleep(10);
-	}
+
 	Sleep(1000);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void ModuleMain(LocalModule& lm) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
--- AsyncGameEngine/AsyncGame.h
+++ AsyncGameEngine/AsyncGame.h
@@ -5,116 +5,118 @@
 #include <atomic>
 #include <array>
 #include <vector>
+#include <cassert>
+#include <assert.h>
+#include <string>
+#include <unordered_map>
 
-class SharedBuffer
+typedef unsigned int uint;
+typedef unsigned short ushort;
+const uint MESSAGE_QUEUE_LEN = 1 * 1024 * 1024;//we'll need to handle this being set by the caller, for compatibility with different versions
+
+std::vector<wchar_t> CharToWChar(std::string &s)
 {
+	int wlen = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, NULL, 0);
+	std::vector<wchar_t> wstr;
+	wstr.resize(wlen);
+	MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, wstr.data(), wlen);
+	return wstr;
+}
+
+struct MessageBlockHeader {
+	ushort len;
+	std::atomic<ushort> read_count;
+	uint sender_id;
+
+	MessageBlockHeader() : read_count(0) {
+	}
+};
+
+struct MessageHeader {
+	ushort type;//maybe this type should be extremely generic and part of the protocol? like { Base, KeyValue, JSON, Sender, Targeted, SenderAndTarget, MultiTarget }
+	ushort len;
+
+	MessageHeader() {
+
+	}
+};
+
+class Message {
+public:
+	MessageHeader header;
+	byte* data;
+
+	Message(byte *d) : data(d) {
+
+	}
+};
+
+class MessageBlock {
+public:
+	MessageBlockHeader header;
 private:
-	//lock
+	byte data[MESSAGE_QUEUE_LEN];
+	//ushort start;
 
-	std::atomic<int>* _get_mutex()
-	{
-		std::atomic<int>* p = (std::atomic<int>*)buff;
-		p--;
-		return p;
-
-		/*std::mutex* p = (std::mutex*)buff;
-		p--;*/
-		//std::mutex& m = *p;
-		return p;
-	}
-
-	void _lock()
-	{
-		auto p = _get_mutex();
-		while (p->exchange(1) != 0) {
-			//lock using the PID instead of the number 1, then I can make it reentrant
-			//I'll need to switch the atomic function to do a compare and swap
-			//if debug, keep a counter and warn on long waits
-		}
-	}
-
-	void _unlock()
-	{
-		auto p = _get_mutex();
-		p->exchange(0);
-	}
-
-	void* buff;
 public:
-	/*SharedBuffer(SharedBuffer&& sb)
-	{
-		buff = sb.buff;
-		sb.buff = NULL;
+
+	Message get(uint &pos) {
+		assert(pos < header.len);
+		Message m(data + pos + sizeof(MessageHeader));
+		m.header = *(MessageHeader*)(data + pos);
+		return m;
 	}
 
-	SharedBuffer(SharedBuffer& sb)
-	{
-		buff = sb.buff;
-		sb.buff = NULL;
-	}*/
+	void push(Message m) {
 
-	SharedBuffer(void* b)
-	{
-		//lock
-		std::atomic<int>* m = (std::atomic<int>*)b;
-		buff = (void*)(m + 1);
-		_lock();
-	}
-
-	void Unlock()
-	{
-		if (buff == NULL) return;
-
-		_unlock();
-		buff = NULL;
-	}
-
-	~SharedBuffer()
-	{
-		if (buff == NULL) return;
-		std::cerr << "you need to release the SharedBuffer and swap\n";
-		Unlock();
-		//unlock
-		//flip buffer? or maybe make it explicit?
-	}
-
-	/*operator void*()
-	{
-		return buff;
-	}*/
-
-	unsigned int GetSize()
-	{
-		return *(unsigned int*)buff;
-	}
-
-	void SetSize(unsigned int size)
-	{
-		*(unsigned int*)buff = size;
-	}
-
-	byte* GetBytes()
-	{
-		return (byte*)buff + sizeof(unsigned int);
 	}
 };
 
-class Command
-{
+class MessageQueue {
+	//circular buffer? can I make it loop to the beginning early if the queue has room at the front in order to improve cache use? basically a dynamically sized circular buffer
+	//the hub module would just reset end to 0 when appropriate? but that means readers will need to check both if the next spot is a new message and if the start is a new message
+
+	//uint protocol_version;?
+	std::atomic<uint> end;
+	byte data[MESSAGE_QUEUE_LEN * 2];//double length so we don't need to strictly wrap based on the length of the MessageBlock being added, we just wrap by the start point
+
 public:
-	std::string command;
+	MessageQueue() : end(0) {
+		assert(end.is_lock_free());
+		MessageBlockHeader mb;
+		assert(mb.read_count.is_lock_free());
+		memset(data, 0, sizeof(data));
+	}
+
+	void SendMessageBlock(MessageBlockHeader block, byte* data)
+	{
+		//assert the lengths add up
+		//assert the types of the messages are recognized?
+		//need to check if the queue has room, and wait until it does
+		uint old_end = end.fetch_add(block.len, std::memory_order::memory_order_release);
+		old_end %= MESSAGE_QUEUE_LEN;
+		memcpy(data + old_end, &block, sizeof(block));
+		memcpy(data + old_end + sizeof(block), data, block.len);
+		MessageBlockHeader* written_block = (MessageBlockHeader*)(data + old_end);
+		written_block->read_count.store(1, std::memory_order::memory_order_release);
+	}
+
+	MessageBlock* GetBlock(uint &pos)
+	{
+		//should I return in-place or a copy? I wonder if in-place will cause issues with atmoic variable updates having to propogate to the caches on other cores
+		//pretty sure incrementing the atomic read_count will invalidate that whole cache line for every other cache
+		//maybe a future optimization could be have a separate array for read_counts to keep them in a different cache line from the data
+		//I should benchmark all the options with many processes doing pings
+		assert(pos < MESSAGE_QUEUE_LEN);
+		MessageBlock* block = (MessageBlock*)(data + pos);
+		ushort read_count = block->header.read_count;
+		if (read_count == 0) return NULL;
+		pos = (pos + block->header.len) % MESSAGE_QUEUE_LEN;
+		return block;
+	}
 };
 
-class CommandBlock
-{
-public:
-	//build up a list of commands to send, on destructor copy them to the shared memory
-	std::vector<Command> commands;
-};
-
-
-class SharedMemory
-{
+class SharedMem {
 	static void* MapFile(HANDLE hMapFile, int BUF_SIZE)
 	{
 		void* pBuf = MapViewOfFile(hMapFile,   // handle to map object
@@ -132,21 +134,16 @@ class SharedMemory
 		return pBuf;
 	}
 
-	HANDLE hMapToEngine[2];
-	void* pBufToEngine[2];
-
-	HANDLE hMapFromEngine[2];
-	void* pBufFromEngine[2];
-
-	int ToEngineBackBuffer;
-	int FromEngineBackBuffer;
+	HANDLE hMap;
+	void* pBuf;
 
 public:
 
-	void CreateSharedMemory(TCHAR *name, HANDLE &hMapFile, void* &pBuf)
+	void* CreateSharedMemory(std::string path, HANDLE& hMapFile, void*& pBuf, uint BUF_SIZE)
 	{
-		const int BUF_SIZE = 4 * 1024 * 1024;
-		std::wcout << "creating shared memory " << std::wstring(name) << "\n";
+		std::cout << "creating shared memory " << path << "\n";
+
+		auto wstr = CharToWChar(path);
 
 		hMapFile = CreateFileMapping(
 			INVALID_HANDLE_VALUE,    // use paging file
@@ -154,7 +151,7 @@ public:
 			PAGE_READWRITE,          // read/write access
 			0,                       // maximum object size (high-order DWORD)
 			BUF_SIZE,                // maximum object size (low-order DWORD)
-			name);                   // name of mapping object
+			wstr.data());            // name of mapping object
 
 		if (hMapFile == NULL)
 		{
@@ -162,38 +159,22 @@ public:
 		}
 		pBuf = MapFile(hMapFile, BUF_SIZE);
 
-		//new (pBuf) std::mutex();
-		new (pBuf) std::atomic<int>(0);
+		//new (pBuf) std::atomic<int>(0);
 
-		std::wcout << "shared memory " << std::wstring(name) << " created!\n";
+		std::cout << "shared memory " << path << " created!\n";
+		return pBuf;
 	}
 
-	void CreateSharedMemory(int shared_mem_id)
+	void* OpenSharedMemory(std::string path, HANDLE& hMapFile, void*& pBuf, uint BUF_SIZE)
 	{
-		TCHAR name[1024];
-		_stprintf_s(name, 1024, TEXT("AsyncGame-ToEngine0-%d"), shared_mem_id);
-		CreateSharedMemory(name, hMapToEngine[0], pBufToEngine[0]);
-		_stprintf_s(name, 1024, TEXT("AsyncGame-ToEngine1-%d"), shared_mem_id);
-		CreateSharedMemory(name, hMapToEngine[1], pBufToEngine[1]);
+		std::cout << "opening shared memory " << path << "\n";
 
-		_stprintf_s(name, 1024, TEXT("AsyncGame-FromEngine0-%d"), shared_mem_id);
-		CreateSharedMemory(name, hMapFromEngine[0], pBufFromEngine[0]);
-		_stprintf_s(name, 1024, TEXT("AsyncGame-FromEngine1-%d"), shared_mem_id);
-		CreateSharedMemory(name, hMapFromEngine[1], pBufFromEngine[1]);
-
-		ToEngineBackBuffer = 0;
-		FromEngineBackBuffer = 0;
-	}
-
-	void OpenSharedMemory(TCHAR *name, HANDLE& hMapFile, void* &pBuf)
-	{
-		const int BUF_SIZE = 4 * 1024 * 1024;
-		std::wcout << "opening shared memory " << std::wstring(name) << "\n";
+		auto wstr = CharToWChar(path);
 
 		hMapFile = OpenFileMapping(
 			FILE_MAP_ALL_ACCESS,   // read/write access
 			FALSE,                 // do not inherit the name
-			name);               // name of mapping object
+			wstr.data());               // name of mapping object
 
 		if (hMapFile == NULL)
 		{
@@ -201,134 +182,101 @@ public:
 		}
 		pBuf = MapFile(hMapFile, BUF_SIZE);
 
-		std::wcout << "shared memory " << std::wstring(name) << " opened!\n";
+		std::cout << "shared memory " << path << " opened!\n";
+		return pBuf;
 	}
 
-	void OpenSharedMemory(int shared_mem_id)
+	~SharedMem()
 	{
-		TCHAR name[1024];
-		_stprintf_s(name, 1024, TEXT("AsyncGameEngine-%d"), shared_mem_id);
-		_stprintf_s(name, 1024, TEXT("AsyncGame-ToEngine0-%d"), shared_mem_id);
-		OpenSharedMemory(name, hMapToEngine[0], pBufToEngine[0]);
-		_stprintf_s(name, 1024, TEXT("AsyncGame-ToEngine1-%d"), shared_mem_id);
-		OpenSharedMemory(name, hMapToEngine[1], pBufToEngine[1]);
-
-		_stprintf_s(name, 1024, TEXT("AsyncGame-FromEngine0-%d"), shared_mem_id);
-		OpenSharedMemory(name, hMapFromEngine[0], pBufFromEngine[0]);
-		_stprintf_s(name, 1024, TEXT("AsyncGame-FromEngine1-%d"), shared_mem_id);
-		OpenSharedMemory(name, hMapFromEngine[1], pBufFromEngine[1]);
-
-		ToEngineBackBuffer = 0;
-		FromEngineBackBuffer = 0;
-	}
-
-	SharedBuffer LockToEngine()
-	{
-		return SharedBuffer(pBufToEngine[ToEngineBackBuffer]);
-	}
-
-	SharedBuffer LockFromEngine()
-	{
-		return SharedBuffer(pBufFromEngine[FromEngineBackBuffer]);
-	}
-
-	void UnlockToEngine(SharedBuffer& sb)
-	{
-		sb.Unlock();
-		//ToEngineBackBuffer = (ToEngineBackBuffer + 1) % 2;//need to figure out when to swap buffers and how to keep the reader and writer on opposite buffers and keep the commands in order?
-	}
-
-	void UnlockFromEngine(SharedBuffer& sb)
-	{
-		sb.Unlock();
-		//FromEngineBackBuffer = (FromEngineBackBuffer + 1) % 2;
-	}
-
-	CommandBlock GetNextCommands(SharedBuffer& sb)
-	{
-		int bytes = 0;
-		int total_bytes = sb.GetSize();
-		byte* buff = sb.GetBytes();
-		byte* end = buff + total_bytes;
-
-		CommandBlock block;
-
-		while (buff < end) {
-			unsigned int size = *(unsigned int*)buff;
-			buff += sizeof(unsigned int);
-			std::string command((char*)buff, size);
-			Command c;
-			c.command = command;
-			block.commands.push_back(c);
-			buff += size;
-		}
-
-		sb.SetSize(0);
-		return block;
-	}
-
-	CommandBlock GetNextCommandsToEngine()
-	{
-		auto sb = LockToEngine();
-		auto c = GetNextCommands(sb);
-		UnlockToEngine(sb);
-		return c;
-	}
-
-	CommandBlock GetNextCommandsFromEngine()
-	{
-		auto sb = LockFromEngine();
-		auto c = GetNextCommands(sb);
-		UnlockFromEngine(sb);
-		return c;
-	}
-
-	void SendCommands(SharedBuffer &sb, CommandBlock &cb)
-	{
-		int bytes = sb.GetSize();
-		byte* buff = sb.GetBytes();
-		buff += bytes;
-
-		for (auto& c : cb.commands) {
-			unsigned int len = (unsigned int)c.command.length();
-			*(unsigned int*)buff = len;
-			bytes += sizeof(unsigned int);
-			buff += sizeof(unsigned int);
-			memcpy(buff, c.command.c_str(), len);
-			bytes += len;
-			buff += len;
-		}
-
-		sb.SetSize(bytes);
-	}
-
-	void SendCommandsToEngine(CommandBlock& cb)
-	{
-		if (cb.commands.size() == 0) return;
-		auto sb = LockToEngine();
-		SendCommands(sb, cb);
-		UnlockToEngine(sb);
-	}
-
-	void SendCommandsFromEngine(CommandBlock& cb)
-	{
-		if (cb.commands.size() == 0) return;
-		auto sb = LockFromEngine();
-		SendCommands(sb, cb);
-		UnlockFromEngine(sb);
-	}
-
-	~SharedMemory()
-	{
-		UnmapViewOfFile(pBufToEngine[0]);
-		UnmapViewOfFile(pBufToEngine[1]);
-		UnmapViewOfFile(pBufFromEngine[0]);
-		UnmapViewOfFile(pBufFromEngine[1]);
-		CloseHandle(hMapToEngine[0]);
-		CloseHandle(hMapToEngine[1]);
-		CloseHandle(hMapFromEngine[0]);
-		CloseHandle(hMapFromEngine[1]);
-
+		assert(pBuf != NULL);
+		assert(hMap != NULL);
+		UnmapViewOfFile(pBuf);
+		CloseHandle(hMap);
+		pBuf = NULL;
+		hMap = NULL;
 		std::cout << "shared memory destroyed!";
 	}
 };
+
+class Module {
+public:
+	uint id;
+	PROCESS_INFORMATION pi;
+
+	static Module SpawnModule(std::string name) {
+		Module spawned = Module();
+		uint shared_mem_id = GetCurrentProcessId();
+		uint pid = spawned.SpawnProcess(name, shared_mem_id);
+		return spawned;
+	}
+
+	uint SpawnProcess(std::string path, uint shared_mem_id) {
+		STARTUPINFO si;
+
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+		ZeroMemory(&pi, sizeof(pi));
+
+		auto wstr = CharToWChar(path);
+		WCHAR wpath[4096];
+		_stprintf_s(wpath, 4096, TEXT("%s %u"), wstr.data(), shared_mem_id);
+
+		bool ret = CreateProcess(NULL,   // No module name (use command line)
+			wpath,        // Command line
+			NULL,           // Process handle not inheritable
+			NULL,           // Thread handle not inheritable
+			FALSE,          // Set handle inheritance to FALSE
+			0,              // No creation flags
+			NULL,           // Use parent's environment block
+			NULL,           // Use parent's starting directory 
+			&si,            // Pointer to STARTUPINFO structure
+			&pi);           // Pointer to PROCESS_INFORMATION structure
+
+		if (!ret) {
+			throw std::system_error(std::error_code((int)GetLastError(), std::system_category()), "error spawning process");
+		}
+		return pi.dwProcessId;
+	}
+};
+
+class LocalModule : public Module {
+	//hold data like where we are in the message queue, event listeners and callbacks? performance stats?
+};
+
+void _ParseKeyValue(const char*& c, std::unordered_map<std::string, std::string> &map) {
+	const char* eq = strchr(c, '=') + 1;
+	const char* newline = strchr(c, '\n');
+	const char* cr = strchr(c, '\r');
+	const char* end = newline;
+	if (cr && cr < end) end = cr;
+	if (end == NULL) end = eq + strlen(eq);
+	std::string k(c, eq-c-1);
+	std::string v(eq, end - eq);
+	c = newline;
+	map[k] = v;
+	c = end;
+}
+
+std::unordered_map<std::string, std::string> ParseKeyValues(std::string text) {
+	std::unordered_map<std::string, std::string> map;
+	const char* c = text.c_str();
+	while (*c) {
+		if (*c != '#') {
+			_ParseKeyValue(c, map);
+		}
+		while (*c) {
+			c++;
+			if (c[-1] == '\n') break;
+		}
+	}
+	return map;
+}
+
+void run_lib_tests() {
+	auto map = ParseKeyValues("foo=bar\n#test comment\nsigma=nuts");
+	assert(map.size() == 2);
+	assert(map["foo"] == "bar");
+	assert(map["sigma"] == "nuts");
+
+	MessageQueue mq;
+}
